f0387c88c06687f7d106f70e5aa4b46d1b0d51b1
rinukkusu <rinukkusu@sub-r.de>
2017-07-03 17:06:04 +0200

    fix dumb flag

--- lib/src/native/console-test/console-test.cpp
+++ lib/src/native/console-test/console-test.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window* window = SDL_CreateWindow("test", 0, 0, 100, 100, SDL_VIDEO_OPENGL);
+	SDL_Window* window = SDL_CreateWindow("test", 30, 30, 100, 100, SDL_WINDOW_OPENGL);
 
     return 0;
 }
