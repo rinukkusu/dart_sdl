6a9569982db422e2bc2a3c2eb927905277d5a461
rinukkusu <rinukkusu@sub-r.de>
2017-07-08 12:35:21 +0200

    add mousemotion and quit event

--- lib/src/native/sdl_extension_lib/sdl_event_map.cpp
+++ lib/src/native/sdl_extension_lib/sdl_event_map.cpp
@@ -5,11 +5,24 @@
 #include "json.h"
 
 
-void EncodeWindowEvent(json_object* o, SDL_WindowEvent* event) {
-	o->add("type", SDL_WINDOWEVENT);
+void EncodeSubEvent(json_object* o, SDL_WindowEvent* event) {
 	o->add("event", event->event);
 	o->add("data1", event->data1);
 	o->add("data2", event->data2);
+	o->add("timestamp", event->timestamp);
+}
+
+void EncodeSubEvent(json_object* o, SDL_QuitEvent* event) {
+	o->add("timestamp", event->timestamp);
+}
+
+void EncodeSubEvent(json_object* o, SDL_MouseMotionEvent* event) {
+	o->add("x", event->x);
+	o->add("xrel", event->xrel);
+	o->add("y", event->y);
+	o->add("yrel", event->yrel);
+	o->add("state", event->state);
+	o->add("which", event->which);
 	o->add("timestamp", event->timestamp);
 }
 
@@ -18,10 +31,18 @@ const char* EncodeEvent(SDL_Event* event) {
 	json_object o = json_object_t;
 	o.init();
 
+	o.add("type", event->type);
+
 	switch (event->type)
 	{
 	case SDL_WINDOWEVENT:
-		EncodeWindowEvent(&o, &event->window);
+		EncodeSubEvent(&o, &event->window);
+		break;
+	case SDL_MOUSEMOTION:
+		EncodeSubEvent(&o, &event->motion);
+		break;
+	case SDL_QUIT:
+		EncodeSubEvent(&o, &event->quit);
 		break;
 	default:
 		break;
